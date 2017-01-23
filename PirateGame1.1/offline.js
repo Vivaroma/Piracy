85fdeba7df214d89f283792384b4b9a7b7940c26
Vivaroma <dillanb2010@hotmail.com>
2017-01-22 18:39:51 -0800

    Add files via upload

---
+++ PirateGame1.1/offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1485139201,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/tiledbackground.png",
+		"images/gunshipsheet-sheet0.png",
+		"images/theravagersheet-sheet0.png",
+		"images/friendlycannonball-sheet0.png",
+		"images/manofwarsheet-sheet0.png",
+		"images/newpiskel4sheet-sheet0.png",
+		"images/deathisland-sheet0.png",
+		"images/gunshipcannon1-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/mowhbar-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/newpiskel-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"pathfind.js"
+	]
+}
